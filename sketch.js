cf20ed5562792323f7e468e1b05e8f1e003d60a5
Joseph Fischetti <fischettijw@gmail.com>
2021-02-27 06:08:51 -0500

    let maxStringLenght = 75;

--- sketch.js
+++ sketch.js
@@ -64,7 +64,7 @@ class Particle {
     // }
 
     addStringToParticles(particles, me) {
-        let maxStringLenght = 25;
+        let maxStringLenght = 75;
         let lineColor = 'red';
         for (let i = 0; i < particles.length; i++) {
             let d = dist(this.pos.x, this.pos.y, particles[i].pos.x, particles[i].pos.y);
